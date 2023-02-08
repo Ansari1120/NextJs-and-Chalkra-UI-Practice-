c14b1d9c17fd8c6e381820187a100bd476fd3716
Ahmed Ali Ansari <ahmedaliansari506@gmail.com>
2023-02-08 23:23:15 +0500

    "practicing chakra ui"

--- chakra-and-next-part-03/app/page.tsx
+++ chakra-and-next-part-03/app/page.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <h1>hey there with nextjs and chakra</h1>
-      </div>
-    </main>
+    // <main className={styles.main}>
+    // <div className={styles.description}>
+    <h1>hey there with nextjs and chakra</h1>
+    //   </div>
+    // </main>
   );
 }
