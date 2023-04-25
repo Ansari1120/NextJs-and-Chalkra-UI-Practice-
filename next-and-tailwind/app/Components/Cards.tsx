d77432f37f5ae2515f33404abb9546ac4bef6761
Ahmed Ali Ansari <ahmedaliansari506@gmail.com>
2023-04-25 23:09:44 +0500

    "cards component"

---
+++ next-and-tailwind/app/Components/Cards.tsx
@@ -0,0 +1,209 @@
+import Link from "next/link";
+import React from "react";
+
+const Cards = () => {
+  return (
+    <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-x-8 gap-y-8 mt-10 mx-8 p-8">
+      <div className="bg-gray-200 rounded-xl">
+        <img
+          className="rounded-t-xl"
+          src="https://picsum.photos/400/300"
+          alt="landscape"
+        />
+        <div className="p-4">
+          <h1 className="font-bold text-2xl">Heading</h1>
+          <p className="mt-4 mb-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quasi
+            possimus commodi expedita dignissimos veritatis quae illum! Omnis
+            quo aliquam dicta voluptatem veniam. Adipisci itaque, consectetur
+            quis omnis voluptatum natus.
+          </p>
+          <Link
+            className="text-md font-semibold bg-slate-500 p-2 rounded-md text-white"
+            href={""}
+          >
+            Read More
+          </Link>
+        </div>
+      </div>
+      <div className="bg-gray-200 rounded-xl">
+        <img
+          className="rounded-t-xl"
+          src="https://picsum.photos/400/300"
+          alt="landscape"
+        />
+        <div className="p-4">
+          <h1 className="font-bold text-2xl">Heading</h1>
+          <p className="mt-4 mb-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quasi
+            possimus commodi expedita dignissimos veritatis quae illum! Omnis
+            quo aliquam dicta voluptatem veniam. Adipisci itaque, consectetur
+            quis omnis voluptatum natus.
+          </p>
+          <Link
+            className="text-md font-semibold bg-slate-500 p-2 rounded-md text-white"
+            href={""}
+          >
+            Read More
+          </Link>
+        </div>
+      </div>
+      <div className="bg-gray-200 rounded-xl">
+        <img
+          className="rounded-t-xl"
+          src="https://picsum.photos/400/300"
+          alt="landscape"
+        />
+        <div className="p-4">
+          <h1 className="font-bold text-2xl">Heading</h1>
+          <p className="mt-4 mb-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quasi
+            possimus commodi expedita dignissimos veritatis quae illum! Omnis
+            quo aliquam dicta voluptatem veniam. Adipisci itaque, consectetur
+            quis omnis voluptatum natus.
+          </p>
+          <Link
+            className="text-md font-semibold bg-slate-500 p-2 rounded-md text-white"
+            href={""}
+          >
+            Read More
+          </Link>
+        </div>
+      </div>
+      <div className="bg-gray-200 rounded-xl">
+        <img
+          className="rounded-t-xl"
+          src="https://picsum.photos/400/300"
+          alt="landscape"
+        />
+        <div className="p-4">
+          <h1 className="font-bold text-2xl">Heading</h1>
+          <p className="mt-4 mb-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quasi
+            possimus commodi expedita dignissimos veritatis quae illum! Omnis
+            quo aliquam dicta voluptatem veniam. Adipisci itaque, consectetur
+            quis omnis voluptatum natus.
+          </p>
+          <Link
+            className="text-md font-semibold bg-slate-500 p-2 rounded-md text-white"
+            href={""}
+          >
+            Read More
+          </Link>
+        </div>
+      </div>
+      <div className="bg-gray-200 rounded-xl">
+        <img
+          className="rounded-t-xl"
+          src="https://picsum.photos/400/300"
+          alt="landscape"
+        />
+        <div className="p-4">
+          <h1 className="font-bold text-2xl">Heading</h1>
+          <p className="mt-4 mb-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quasi
+            possimus commodi expedita dignissimos veritatis quae illum! Omnis
+            quo aliquam dicta voluptatem veniam. Adipisci itaque, consectetur
+            quis omnis voluptatum natus.
+          </p>
+          <Link
+            className="text-md font-semibold bg-slate-500 p-2 rounded-md text-white"
+            href={""}
+          >
+            Read More
+          </Link>
+        </div>
+      </div>
+      <div className="bg-gray-200 rounded-xl">
+        <img
+          className="rounded-t-xl"
+          src="https://picsum.photos/400/300"
+          alt="landscape"
+        />
+        <div className="p-4">
+          <h1 className="font-bold text-2xl">Heading</h1>
+          <p className="mt-4 mb-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quasi
+            possimus commodi expedita dignissimos veritatis quae illum! Omnis
+            quo aliquam dicta voluptatem veniam. Adipisci itaque, consectetur
+            quis omnis voluptatum natus.
+          </p>
+          <Link
+            className="text-md font-semibold bg-slate-500 p-2 rounded-md text-white"
+            href={""}
+          >
+            Read More
+          </Link>
+        </div>
+      </div>
+      <div className="bg-gray-200 rounded-xl">
+        <img
+          className="rounded-t-xl"
+          src="https://picsum.photos/400/300"
+          alt="landscape"
+        />
+        <div className="p-4">
+          <h1 className="font-bold text-2xl">Heading</h1>
+          <p className="mt-4 mb-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quasi
+            possimus commodi expedita dignissimos veritatis quae illum! Omnis
+            quo aliquam dicta voluptatem veniam. Adipisci itaque, consectetur
+            quis omnis voluptatum natus.
+          </p>
+          <Link
+            className="text-md font-semibold bg-slate-500 p-2 rounded-md text-white"
+            href={""}
+          >
+            Read More
+          </Link>
+        </div>
+      </div>
+      <div className="bg-gray-200 rounded-xl">
+        <img
+          className="rounded-t-xl"
+          src="https://picsum.photos/400/300"
+          alt="landscape"
+        />
+        <div className="p-4">
+          <h1 className="font-bold text-2xl">Heading</h1>
+          <p className="mt-4 mb-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quasi
+            possimus commodi expedita dignissimos veritatis quae illum! Omnis
+            quo aliquam dicta voluptatem veniam. Adipisci itaque, consectetur
+            quis omnis voluptatum natus.
+          </p>
+          <Link
+            className="text-md font-semibold bg-slate-500 p-2 rounded-md text-white"
+            href={""}
+          >
+            Read More
+          </Link>
+        </div>
+      </div>
+      <div className="bg-gray-200 rounded-xl">
+        <img
+          className="rounded-t-xl"
+          src="https://picsum.photos/400/300"
+          alt="landscape"
+        />
+        <div className="p-4">
+          <h1 className="font-bold text-2xl">Heading</h1>
+          <p className="mt-4 mb-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quasi
+            possimus commodi expedita dignissimos veritatis quae illum! Omnis
+            quo aliquam dicta voluptatem veniam. Adipisci itaque, consectetur
+            quis omnis voluptatum natus.
+          </p>
+          <Link
+            className="text-md font-semibold bg-slate-500 p-2 rounded-md text-white"
+            href={""}
+          >
+            Read More
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
--- next-and-tailwind/app/page.tsx
+++ next-and-tailwind/app/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Tweet from "./Components/Tweet"
+import Cards from "./Components/Cards";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-   <>
-   <Tweet />
-   </>
+    <>
+      <Cards />
+    </>
   );
 }
